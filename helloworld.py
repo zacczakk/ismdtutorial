90587a2e383d7d0e04ed94210120ca926448aceb
Philipp Rosendahl <zacc@live.de>
2021-03-12 15:34:51 +0100

    Add python miniscript

---
+++ helloworld.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+"""
+Python miniscript for the ISM+D git workshop. Run by entering
+
+  $ python helloworld.py
+
+Author: Philipp Rosendahl, rosendahl@2phi.de
+Date: 03/2021
+"""
+
+printstring = 'Hello world.'
+print(printstring)
